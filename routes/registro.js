0f789d6844b65cd4f0f8ba53dbb714f1696decd7
Gema Patricia Herrera de Miguel <gp.herrera@usp.ceu.es>
2022-12-26 00:15:51 +0100

    Registro e inicio sesión v2

--- routes/registro.js
+++ routes/registro.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('registro', { title: 'Registro' });
+  res.render('registro', { title: 'Registro de nuevos usuarios' });
 });
 
 module.exports = router;
