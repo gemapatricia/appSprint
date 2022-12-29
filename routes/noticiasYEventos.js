db9ce70b60d3643ad60be78a784a1a70fcb1cd5c
Ramón Merchán Castillo <r.merchan@usp.ceu.es>
2022-12-30 00:17:10 +0100

    enlace en el navbar de noticias

--- routes/noticiasYEventos.js
+++ routes/noticiasYEventos.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('noticias', { title: 'Noticias y eventos' });
+  res.render('noticiasYEventos', { title: 'Noticias y eventos' });
 });
 
 module.exports = router;
