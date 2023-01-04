63da925f2f02dfeedb4cace3dda0388d7bd033d1
Elena Cores <e.cores@usp.ceu.es>
2023-01-04 21:47:50 +0100

    hide comment box a usuarios no premium

--- routes/deporteConcreto.js
+++ routes/deporteConcreto.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var database = require('../database'); 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('deporteConcreto', { title: 'Deportes', user: req.session.user, rol: req.session.rol });
 });
+
 
 module.exports = router;
